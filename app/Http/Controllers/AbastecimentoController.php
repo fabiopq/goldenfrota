bde0b6842071ee16b63d2f0aff84794702e35c7f
fabiopq <fabiopq@gmail.com>
2022-07-16 10:18:42 -0300

    .

--- app/Http/Controllers/AbastecimentoController.php
+++ app/Http/Controllers/AbastecimentoController.php
@@ -875,7 +875,7 @@ class AbastecimentoController extends Controller
     public function apiAbastecimentos()
     {
         return response()->json(DB::table('abastecimentos')
-            ->select('abastecimentos.*')
+            ->select('abastecimentos.*', 'veiculos.placa')
             ->leftJoin('bicos', 'bicos.id', 'abastecimentos.bico_id')
             ->leftJoin('veiculos', 'veiculos.id', 'abastecimentos.veiculo_id')
             ->leftJoin('atendentes', 'atendentes.id', 'abastecimentos.atendente_id')
