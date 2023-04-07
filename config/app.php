699997087bcf70e58b996131f9caeff73e934808
fabiopq <fabiopq@gmail.com>
2023-04-07 16:06:12 -0300

    .

--- config/app.php
+++ config/app.php
@@ -165,7 +165,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Khill\Lavacharts\Laravel\LavachartsServiceProvider::class,
+        
         
 
         /*
@@ -231,8 +231,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
-        //'Lava' => Khill\Lavacharts\Laravel\LavachartsFacade::class,
-        'Lava' => 'Khill\Lavacharts\Laravel\LavachartsFacade',
     ],
 
 ];
