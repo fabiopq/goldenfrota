6d5f455eb023dd0c39031f70f4f8bb87b269e0be
fabiopq <fabiopq@gmail.com>
2023-05-27 22:51:27 -0300

    .

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
         'api' => [
             'throttle:60,1',
             'bindings',
-            //\App\Http\Middleware\BasicAuth::class,
+            \App\Http\Middleware\BasicAuth::class,
         ],
     ];
 
