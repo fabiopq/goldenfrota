a54aecf33562fd1c78af926212d266d8631d0f7b
fabiopq <fabiopq@email.com>
2025-04-08 20:01:30 -0300

    .

--- app/Http/Controllers/TicketstatusController.php
+++ app/Http/Controllers/TicketstatusController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 
-class TicketstatusController extends Controller
+class TicketStatusController extends Controller
 {
     public $fields = array(
         'id' => 'ID',
